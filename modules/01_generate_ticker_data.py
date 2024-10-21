5f48954cca7738a09c758c840ce9ea568a24ec9d
saint-vampirdus <keeney.arcadia@gmail.com>
2024-10-21 10:41:12 -0500

    Added functionality to write to a csv. If the data already exists, it is overwritten to always have current data

--- modules/01_generate_ticker_data.py
+++ modules/01_generate_ticker_data.py
@@ -1,11 +1,13 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import os
+import glob
 
 # List of Tickers to pull in (GME and its known ETFS)
 ticker_list = ['GME', 'XRT', 'FNDA', 'IWB', 'IWM', 'IJH', 'VTI', 'VBR', 'VXF']
-start_date = '2016-01-01' #start date of ticker data
-end_date = datetime.now() - timedelta(days=1) # pull in all data up to last close
+start_date = '2016-01-01'  # Start date of ticker data
+end_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')  # Pull in all data up to last close
 
 def generate_ticker_data(ticker_list, start_date, end_date):
     df_list = []
@@ -33,11 +35,31 @@ def generate_ticker_data(ticker_list, start_date, end_date):
 
     return df_tickers
 
+def generate_ticker_csv(df_tickers):
+    # Determine the path where data should be saved
+    data_root = 'data'  # Assuming 'data' is the root folder for data
+    if not os.path.exists(data_root):
+        os.makedirs(data_root)
+
+    # Generate filename
+    filename = f'ticker-data-{start_date}-{end_date}.csv'
+    full_path = os.path.join(data_root, filename)
+
+    # Delete existing files that start with 'ticker-data-'
+    for file in glob.glob(os.path.join(data_root, 'ticker-data-*.csv')):
+        os.remove(file)
+
+    # Export DataFrame to CSV
+    df_tickers.to_csv(full_path, index=False)
+
+    print(f"Data exported to {full_path}")
+
 def main():
     df_tickers = generate_ticker_data(ticker_list=ticker_list, 
                                       start_date=start_date,
                                       end_date=end_date)
-    print(df_tickers.tail())
+    
+    generate_ticker_csv(df_tickers)
 
 if __name__ == "__main__":
     main()
